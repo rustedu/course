7146ab8fce0655a47970e515747f7a40fab36d31
Li Ming <limingth@gmail.com>
2022-11-08 09:56:08 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,21 +1,21 @@
 export default {
-  clientId: 385,
-  logo: 'https://ssl.cdn.maodouketang.com/Fpkgonzaw5GTUFa0Bfvd_ZlO5yq1',
-  title: '开源操作系统社区',
-  subTitle: '促进操作系统的教学、研究与产业的发展',
+  clientId: 450,
+  logo: 'https://ssl.cdn.maodouketang.com/Fi65zYOF9bcEIjo5ZYDrKuosUSiE',
+  title: '阿图教育',
+  subTitle: '致力于做中国最好的Rust培训品牌',
   footer: {
     resources: [
       {
-        text: 'Rust基础教程',
-        link: 'http://docs.os2edu.cn/'
+        text: 'Rust程序设计语言',
+        link: 'https://kaisery.github.io/trpl-zh-cn/'
       },
       {
-        text: '清华rCore操作系统实践教程',
-        link: 'http://rcore-os.cn/rCore-Tutorial-Book-v3/'
+        text: 'Rust语言圣经',
+        link: 'https://course.rs/'
       },
       {
-        text: '全国大学生操作系统设计大赛',
-        link: 'https://os.educg.net/2022CSCC'
+        text: '通过例子学 Rust',
+        link: 'https://rustwiki.org/zh-CN/rust-by-example/'
       }
     ],
     contact: [
